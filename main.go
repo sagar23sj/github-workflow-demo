2fa9cb6fd0cb44c2477af6170ac162fa4fa4dcea
sagar23sj <sagarsonwane23@gmail.com>
2021-06-10 01:58:25 +0530

    Intentionally failing go vet

--- main.go
+++ main.go
@@ -9,7 +9,16 @@ func PrintTime() string {
 	return fmt.Sprintf("Current Time Is %s: ", time.Now())
 }
 
+func processValue(i int) {
+	fmt.Println(i)
+}
+
 func main() {
+	for i := 0; i < 10; i++ {
+		go func() {
+			processValue(i)
+		}()
+	}
 	fmt.Println(PrintTime())
 
 }
